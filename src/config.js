58de4c8b03f3f1fd5284245ca9cda9342400b531
Carlo Costantino <costantinoart@gmail.com>
2020-02-16 12:32:24 -1000

    Setup config file for env variables

---
+++ src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  NODE_ENV: process.env.NODE_ENV || "development",
+  PORT: process.env.PORT || 8000
+  // DB_URL:
+  //   process.env.DB_URL || "postgresql://dunder_mifflin@localhost/bookmarks"
+};
