8c7e7929d62181dee09259bb4c3f4f7134115ed4
Carlo Costantino <costantinoart@gmail.com>
2020-02-17 23:26:43 +0000

    Added note/folder fixtures and implimented GET /api/notes endpoint

---
+++ test/folder-fixtures.js
@@ -0,0 +1,20 @@
+function makeFolderArray() {
+  return [
+    {
+      name: "Important"
+    },
+    {
+      name: "Super"
+    },
+    {
+      name: "Spangley"
+    },
+    {
+      name: "Carlo Test"
+    }
+  ];
+}
+
+module.exports = {
+  makeFolderArray
+};
--- test/note-endpoint-spec.js
+++ test/note-endpoint-spec.js
@@ -1,5 +1,7 @@
 const knex = require("knex");
 const app = require("../src/app");
+const { makeFolderArray } = require("./folder-fixtures");
+const { makeNoteArray } = require("./note-fixtures");
 
 describe("Articles Endpoints", function() {
   let db;
@@ -14,13 +16,16 @@ describe("Articles Endpoints", function() {
 
   after("disconnect from db", () => db.destroy());
 
-  before("clean the table", () =>
-    db.raw("TRUNCATE note, folder RESTART IDENTITY CASCADE")
-  );
+  before("clean the table", () => {
+    db.raw("TRUNCATE folder, note RESTART IDENTITY CASCADE");
+    console.log("before cleanup ran");
+  });
 
-  afterEach("cleanup", () =>
-    db.raw("TRUNCATE note, folder RESTART IDENTITY CASCADE")
-  );
+  afterEach("cleanup", () => {
+    db.raw("TRUNCATE folder, note RESTART IDENTITY CASCADE");
+    console.log("afterEach cleanup ran");
+  });
+  afterEach("cleanup", () => db("note").truncate());
 
   describe(`GET /api/notes`, () => {
     context(`Given no notes`, () => {
@@ -28,6 +33,27 @@ describe("Articles Endpoints", function() {
         return supertest(app)
           .get("/api/notes")
           .expect(200, []);
+      });
+    });
+    context("Given there are articles in the database", () => {
+      const testFolders = makeFolderArray();
+      const testNotes = makeNoteArray();
+
+      beforeEach("insert notes", () => {
+        console.log("beforeEach insert notes was triggered");
+        return db
+          .into("folder")
+          .insert(testFolders)
+          .then(() => {
+            console.log("insert testNotes ran");
+            return db.into("note").insert(testNotes);
+          });
+      });
+
+      it("GET /api/notes responds with 200 and all of the notes", () => {
+        return supertest(app)
+          .get("/api/notes")
+          .expect(200, testNotes);
       });
     });
   });
